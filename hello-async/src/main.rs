800ef61481a861810bf4fb1c3f4fe01a04e9a247
stiproot <stipcich.simon@gmail.com>
2025-05-08 08:14:39 +0200

    feat: more on async.

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,14 +1,50 @@
-use trpl::Html;
+// use trpl::Html;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    let tx_clone = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("from"),
+            String::from("another thread"),
+        ];
+
+        for val in vals {
+            tx_clone.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
 
-async fn page_title(url: &str) -> Option<String> {
-    let resp = trpl::get(url).await;
-    let resp_txt = resp.text().await;
-
-    Html::parse(&resp_txt)
-        .select_first("title")
-        .map(|title_element| title_element.inner_html())
-}
+// async fn page_title(url: &str) -> (&str, Option<String>) {
+//     let txt = trpl::get(url).await.text().await;
+//     let title = Html::parse(&txt)
+//         .select_first("title")
+//         .map(|title_element| title_element.inner_html());
+//     (url, title)
+// }
